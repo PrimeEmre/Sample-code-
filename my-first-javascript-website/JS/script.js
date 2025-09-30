bf53a3c13ec0afd463a146ead97b2e13ac726d54
PrimeEmre <ynsemrgzl@gmail.com>
2025-09-30 11:31:26 -0400

    fisned the arrays

---
+++ my-first-javascript-website/JS/script.js
@@ -0,0 +1,14 @@
+let carList = ["Mercedes", "BMW", "Rolls Royce ", "Ford"]
+const cloriList = ["Chips:" +" "+ 300 + " ", "Penuts" + " " + 280 + " " + " " , "Chocolate" + " "+ 500 + " ", "Candy" +" " + 150 + " " ]
+let fruitList = [" Apple", "Banana", "Orange", "Grapes"]
+let decimalList = [1.2, 2.3, 3.4, 4.5]
+
+function helloWorld() {
+alert(cloriList )
+}
+
+console.log(carList)
+console.log(cloriList)
+console.log(fruitList)
+console.log(decimalList)
+
