5d2551c9973c57aa089a0d5db6a31d7e6c8f1dbf
PrimeEmre <ynsemrgzl@gmail.com>
2025-09-30 15:50:15 -0400

    finshed the age movie theraterh calcualtort

--- my-first-javascript-website/JS/script.js
+++ my-first-javascript-website/JS/script.js
@@ -1,16 +1,29 @@
-let carList = ["Mercedes", "BMW", "Rolls Royce ", "Ford"]
-const cloriList = ["Chips:" +" "+ 300 + " ", "Penuts" + " " + 280 + " " + " " , "Chocolate" + " "+ 500 + " ", "Candy" +" " + 150 + " " ]
-let fruitList = [" Apple", "Banana", "Orange", "Grapes"]
-let decimalList = [1.2, 2.3, 3.4, 4.5]
+// let carList = ["Mercedes", "BMW", "Rolls Royce ", "Ford"]
+// const cloriList = ["Chips:" +" "+ 300 + " ", "Penuts" + " " + 280 + " " + " " , "Chocolate" + " "+ 500 + " ", "Candy" +" " + 150 + " " ]
+// let fruitList = [" Apple", "Banana", "Orange", "Grapes"]
+// let decimalList = [1.2, 2.3, 3.4, 4.5]
+let age = 20; 
 
-function helloWorld() {
-alert(cloriList )
+if (age >= 17) {
+    document.getElementById("movie-rating").innerHTML = "You are allowed to watch R-rated movies."
+} else if (age >= 13) {
+    document.getElementById("movie-rating").innerHTML = "You are allowed to watch PG-13 rated movies."
+} else if (age >= 0) {
+    document.getElementById("movie-rating").innerHTML = "You are allowed to watch G and PG-rated movies."
+} else {
+    document.getElementById("movie-rating").innerHTML = "Invalid age. You are not allowed to watch any movies."
 }
 
-console.log(carList)
-console.log(cloriList)
-console.log(fruitList)
-console.log(decimalList)
 
-document.getElementById("all-about-me").innerHTML = "My name is James smith and I am 30 years  old programer form Istanbul "
+
+// function helloWorld() {
+// alert(cloriList )
+// }
+
+// console.log(carList)
+// console.log(cloriList)
+// console.log(fruitList)
+// console.log(decimalList)
+
+// document.getElementById("all-about-me").innerHTML = "My name is James smith and I am 30 years  old programer form Istanbul "
 
