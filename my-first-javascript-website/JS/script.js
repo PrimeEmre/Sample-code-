cd5f550657e4abaea883c4f8184a51ecbaae5a5d
PrimeEmre <ynsemrgzl@gmail.com>
2025-09-30 10:22:28 -0400

    finshed the calcualtions

--- my-first-javascript-website/JS/script.js
+++ my-first-javascript-website/JS/script.js
@@ -1,4 +1,9 @@
+let num1 = 20
+let num2 = 30
+const pi = 3.14
+
 function helloWorld() {
-    alert("Hello World")
+    alert((num1 + num2) * pi)
 }
-console.log("Hello World")
+
+console.log((num1 + num2) * pi)
