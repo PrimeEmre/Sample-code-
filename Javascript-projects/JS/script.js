ad32c23dd72fbf54e988612e754c5cbeca977ddd
PrimeEmre <ynsemrgzl@gmail.com>
2025-10-12 22:41:01 -0400

    formated

--- Javascript-projects/JS/script.js
+++ Javascript-projects/JS/script.js
@@ -6,10 +6,11 @@
 //     let result =  parseInt(num1) + parseInt(num2)
 //     document.getElementById("result").innerHTML = "The result is " + result
 // }
+
 //Area Of Triangle Calculator 
 function calcualte() {
     let baseOfTriangle = document.getElementById("base-of-triangle").value
     let heightOfTriangle = document.getElementById("height-of-triangle").value
     let result = parseInt(baseOfTriangle) * parseInt(heightOfTriangle) / 2
-    document.getElementById("result").innerHTML = "The result is " + result
+    document.getElementById("result").innerHTML = "The result is " + result +" cm²"
 }
