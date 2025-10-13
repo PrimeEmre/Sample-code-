c3312202f48be0ccb1bb1da65fa1afee8cf903c9
PrimeEmre <ynsemrgzl@gmail.com>
2025-10-12 21:10:28 -0400

    finsihed the addition program

---
+++ Javascript-projects/JS/script.js
@@ -0,0 +1,8 @@
+
+
+function calcualte() {
+    let num1 = document.getElementById("num-1").value
+    let num2 =  document.getElementById("num-1").value
+    let result =  parseInt(num1) + parseInt(num2)
+    document.getElementById("result").innerHTML = "The result is " + result
+}
