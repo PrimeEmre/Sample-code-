376d5cbdc9f1455158bcac722e9432a2124b8af1
PrimeEmre <ynsemrgzl@gmail.com>
2025-10-12 22:27:17 -0400

    chnaged the names of the veribles

--- Javascript-projects/JS/script.js
+++ Javascript-projects/JS/script.js
@@ -8,8 +8,8 @@
 // }
 
 function calcualte() {
-    let num1 = document.getElementById("base-of-triangle").value
-    let num2 =  document.getElementById("height-of-triangle").value
-    let result =  parseInt(num1) * parseInt(num2) /2
+    let baseOfTriangle = document.getElementById("base-of-triangle").value
+    let heightOfTriangle =  document.getElementById("height-of-triangle").value
+    let result =  parseInt(baseOfTriangle) * parseInt(heightOfTriangle) /2
     document.getElementById("result").innerHTML = "The result is " + result
 }
