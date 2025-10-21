a743afd500485c5d961509afc766555b60f02c23
PrimeEmre <ynsemrgzl@gmail.com>
2025-10-21 15:29:27 -0400

    finshed the program

--- Javascript-projects/JS/script.js
+++ Javascript-projects/JS/script.js
@@ -16,13 +16,32 @@
 // }
 
 //Fahrenheit to Celsius Converter
+// function convert() {
+//     // Setting up the variables
+//     let fahrenheitValue =  parseFloat (document.getElementById("Fahrenheit").value)
+//     let celsiusInput  = document.getElementById("Celsius")
+//     let celsiusValue  = (fahrenheitValue - 32) * 5/9
+
+//     // calculating the celsius value
+//     celsiusInput.value = celsiusValue.toFixed(2)
+//     document.getElementById("formula").innerHTML = "Formula: (" + fahrenheitValue + " - " + 32 + ") X 5/9"
+// }
+
 function convert() {
     // Setting up the variables
-    let fahrenheitValue =  parseFloat (document.getElementById("Fahrenheit").value)
-    let celsiusInput  = document.getElementById("Celsius")
-    let celsiusValue  = (fahrenheitValue - 32) * 5/9
-    
-    // calculating the celsius value
-    celsiusInput.value = celsiusValue.toFixed(2)
-    document.getElementById("formula").innerHTML = "Formula: (" + fahrenheitValue + " - " + 32 + ") X 5/9"
+    const taxAmount = 0.18
+    let hourlyWorked = parseFloat(document.getElementById("hourly-worked").value)
+    let hourlyWage = parseFloat(document.getElementById("hourly-wage").value)
+    let takeHomePay = hourlyWorked * hourlyWage * (1 - taxAmount)
+    let taxes = (hourlyWorked * hourlyWage) * taxAmount
+
+    // Correct currency formatting
+    let formattedPrice = new Intl.NumberFormat('en-CA', {
+        style: 'currency',
+        currency: 'CAD',
+    });
+    document.getElementById("result").innerHTML = 'Take Home Pay: ' + formattedPrice.format(takeHomePay) + '<br> Taxes: ' + formattedPrice.format(taxes)
+
+
+
 }
