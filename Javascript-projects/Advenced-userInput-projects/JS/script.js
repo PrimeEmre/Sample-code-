234bd9366633fe7767d34b71f7eb66a2d393b71a
PrimeEmre <ynsemrgzl@gmail.com>
2025-10-24 22:50:07 -0400

    fixed the logic erorr

--- Javascript-projects/Advenced-userInput-projects/JS/script.js
+++ Javascript-projects/Advenced-userInput-projects/JS/script.js
@@ -9,68 +9,38 @@
 //     document.getElementById("vote").innerHTML = "You are old enough to vote"
 // }
 // }
-
 function convert() {
-    let grade = parseInt(document.getElementById("grade").value)
-    if (grade <= 100 && grade >= 95) {
-        document.getElementById("convert").innerHTML = "You got an A+"
+    let grade = parseInt(document.getElementById("grade").value);
+    let result = "" 
+    if (grade >= 95 && grade <= 100) {
+        result = "You got an A+"
+    } else if (grade >= 85 && grade <= 94) {
+        result = "You got an A"
+    } else if (grade >= 80 && grade <= 84) {
+        result = "You got an A-"
+    } else if (grade >= 77 && grade <= 79) {
+        result = "You got a B+"
+    } else if (grade >= 73 && grade <= 76) {
+        result = "You got a B"
+    } else if (grade >= 70 && grade <= 72) {
+        result = "You got a B-"
+    } else if (grade >= 67 && grade <= 69) {
+        result = "You got a C+"
+    } else if (grade >= 63 && grade <= 66) {
+        result = "You got a C"
+    } else if (grade >= 60 && grade <= 62) {
+        result = "You got a C-"
+    } else if (grade >= 57 && grade <= 59) {
+        result = "You got a D+"
+    } else if (grade >= 53 && grade <= 56) {
+        result = "You got a D"
+    } else if (grade >= 50 && grade <= 52) {
+        result = "You got a D-"
+    } else if (grade < 50 && grade >= 0) {
+        result = "You got an R (fail)"
     } else {
-        document.getElementById("convert").innerHTML = "You got an A"
-    }
-    if (grade <= 94 && grade >= 84) {
-        document.getElementById("convert").innerHTML = "You got an A"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an A-"
-    }
-    if (grade <= 86 && grade >= 80) {
-        document.getElementById("convert").innerHTML = "You got an A-"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an B+"
-    }
-    if (grade <= 79 && grade >= 77) {
-        document.getElementById("convert").innerHTML = "You got an B+"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an B"
-    }
-    if (grade <= 76 && grade >= 73) {
-        document.getElementById("convert").innerHTML = "You got an B"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an B-"
-    }
-    if (grade <= 72 && grade >= 70) {
-        document.getElementById("convert").innerHTML = "You got an B-"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an C+"
-    }
-    if (grade <= 69 && grade >= 67) {
-        document.getElementById("convert").innerHTML = "You got an C+"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an C"
-    }
-    if (grade <= 66 && grade >= 63) {
-        document.getElementById("convert").innerHTML = "You got an C"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an C-"
-    }
-    if (grade <= 62 && grade >= 60) {
-        document.getElementById("convert").innerHTML = "You got an C-"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an D+"
-    }
-    if (grade <= 59 && grade >= 57) {
-        document.getElementById("convert").innerHTML = "You got an D+"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an D+"
-    }
-    if (grade <= 53 && grade >= 52) {
-        document.getElementById("convert").innerHTML = "You got an D"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an D-"
-    }
-    if (grade <= 52 && grade >= 50) {
-        document.getElementById("convert").innerHTML = "You got an D-"
-    } else {
-        document.getElementById("convert").innerHTML = "You got an R"
+        result = "Invalid grade entered"
     }
 
+    document.getElementById("convert").innerHTML = result;
 }
