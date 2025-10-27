56e989ad0e0ef15cf0a0ff3a739be1a4de9a5e03
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-10-26 21:27:39 -0400

    finshed the frontend

--- Javascript-projects/Advenced-userInput-projects/JS/script.js
+++ Javascript-projects/Advenced-userInput-projects/JS/script.js
@@ -9,38 +9,41 @@
 //     document.getElementById("vote").innerHTML = "You are old enough to vote"
 // }
 // }
-function convert() {
-    let grade = parseInt(document.getElementById("grade").value)
-    let result = "" 
-    if (grade >= 95 && grade <= 100) {
-        result = "You got an A+"
-    } else if (grade >= 85 && grade <= 94) {
-        result = "You got an A"
-    } else if (grade >= 80 && grade <= 84) {
-        result = "You got an A-"
-    } else if (grade >= 77 && grade <= 79) {
-        result = "You got a B+"
-    } else if (grade >= 73 && grade <= 76) {
-        result = "You got a B"
-    } else if (grade >= 70 && grade <= 72) {
-        result = "You got a B-"
-    } else if (grade >= 67 && grade <= 69) {
-        result = "You got a C+"
-    } else if (grade >= 63 && grade <= 66) {
-        result = "You got a C"
-    } else if (grade >= 60 && grade <= 62) {
-        result = "You got a C-"
-    } else if (grade >= 57 && grade <= 59) {
-        result = "You got a D+"
-    } else if (grade >= 53 && grade <= 56) {
-        result = "You got a D"
-    } else if (grade >= 50 && grade <= 52) {
-        result = "You got a D-"
-    } else if (grade < 50 && grade >= 0) {
-        result = "You got an R (fail)"
-    } else {
-        result = "Invalid grade entered"
-    }
 
-    document.getElementById("convert").innerHTML = result
-}
+//Grade Calculator 
+
+// function convert() {
+//     let grade = parseInt(document.getElementById("grade").value)
+//     let result = "" 
+//     if (grade >= 95 && grade <= 100) {
+//         result = "You got an A+"
+//     } else if (grade >= 85 && grade <= 94) {
+//         result = "You got an A"
+//     } else if (grade >= 80 && grade <= 84) {
+//         result = "You got an A-"
+//     } else if (grade >= 77 && grade <= 79) {
+//         result = "You got a B+"
+//     } else if (grade >= 73 && grade <= 76) {
+//         result = "You got a B"
+//     } else if (grade >= 70 && grade <= 72) {
+//         result = "You got a B-"
+//     } else if (grade >= 67 && grade <= 69) {
+//         result = "You got a C+"
+//     } else if (grade >= 63 && grade <= 66) {
+//         result = "You got a C"
+//     } else if (grade >= 60 && grade <= 62) {
+//         result = "You got a C-"
+//     } else if (grade >= 57 && grade <= 59) {
+//         result = "You got a D+"
+//     } else if (grade >= 53 && grade <= 56) {
+//         result = "You got a D"
+//     } else if (grade >= 50 && grade <= 52) {
+//         result = "You got a D-"
+//     } else if (grade < 50 && grade >= 0) {
+//         result = "You got an R (fail)"
+//     } else {
+//         result = "Invalid grade entered"
+//     }
+
+//     document.getElementById("convert").innerHTML = result
+// }
