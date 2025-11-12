9eb61008a3425185c30446f3089d9b8c55a3e679
PrimeEmre <ynsemrgzl@gmail.com>
2025-11-12 14:43:23 -0500

    almost doen the program

--- Javascript-projects/Advenced-userInput-projects/JS/script.js
+++ Javascript-projects/Advenced-userInput-projects/JS/script.js
@@ -184,6 +184,20 @@
 //     document.getElementById("result").textContent = additionText + " = " + result
 // }
 
+// Sum of n natural numbers
 function calculate() {
-
+    // setting the veribles
+    let num = parseInt(document.getElementById("num").value)
+    let result = 0
+    let additionText = ""
+    // Setting the loop
+    for (num = num; num > 0; num--) {
+        result = result + num;
+        additionText = additionText + num;
+        
+        if (num > 1) {
+            additionText = additionText + " + ";
+        }
+    }
+    document.getElementById("result").textContent = additionText + " = " + result
 }
