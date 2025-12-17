5a734b392db7f88e2b86ac98fefea44dd7cb6138
PrimeEmre <ynsemrgzl@gmail.com>
2025-12-17 14:05:16 -0500

    added lists

--- Python-projects/main.py
+++ Python-projects/main.py
@@ -1,3 +1,9 @@
-print ("hello , world ")
-print(100 + 100) 
-print((200 + 500) *4 +200 - 890)
+# calculation and writng  our first program in python
+# print ("hello , world ")
+# print(100 + 100) 
+# print((200 + 500) *4 +200 - 890)
+
+my_list = [10,20,30,40,50]
+print (len(my_list))
+fruit_list = ['apple:', 100, 'banana:', 50, 'cherry:',75]
+print (type(fruit_list))
