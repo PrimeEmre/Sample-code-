07818cce6232cd9963f9fedc864d44cfd3e2e3bd
PrimeEmre <ynsemrgzl@gmail.com>
2025-12-18 14:14:57 -0500

    finsed off with the lists in pyrhon

--- Python-projects/Basics-Of-Python/main.py
+++ Python-projects/Basics-Of-Python/main.py
@@ -7,19 +7,25 @@
 # num_1 = 100
 # num_2 = 500
 # print (num_1 + num_2)
+
 # name = "Emre"
 # age = 20
 # print ("My name is " + name + " and I am " + str(age) + " years old.")
-age = 30 
-name = "James Smith"
-job = " software developer"
-print  ("my name is" + " " + name +"."+ " " "I am "  + str(age) + " " + "years old." + " My job is" + job +"." )
+
+# age = 30 
+# name = "James Smith"
+# job = " software developer"
+# print  ("my name is" + " " + name +"."+ " " "I am "  + str(age) + " " + "years old." + " My job is" + job +"." )
 
 # lists in python
 # my_list = [10,20,30,40,50]
 # print (len(my_list))
+
 # fruit_list = ['apple:', 100, 'banana:', 50, 'cherry:',75]
 # print (fruit_list)
 
 # car= list (( "bmw", "audi", "toyota", "ferrari"))
 # print (car)
+
+# coding_list = ["python", "java", "c++", "javascript", "ruby"]
+# print (coding_list [2:4])
