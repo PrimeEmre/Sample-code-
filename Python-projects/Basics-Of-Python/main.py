54fbc2d5535d3c3508b8ae076da8e8d9c12c04ee
PrimeEmre <ynsemrgzl@gmail.com>
2025-12-18 15:22:00 -0500

    added another function

--- Python-projects/Basics-Of-Python/main.py
+++ Python-projects/Basics-Of-Python/main.py
@@ -24,22 +24,30 @@
 # fruit_list = ['apple:', 100, 'banana:', 50, 'cherry:',75]
 # print (fruit_list)
 
-# car= list (( "bmw", "audi", "toyota", "ferrari"))
+# car = list (( "bmw", "audi", "toyota", "ferrari"))
 # print (car)
 
 # coding_list = ["python", "java", "c++", "javascript", "ruby"]
 # print (coding_list [2:4])
 
 # Functions 
+
 # def hello():
 #     print ("hello , world ")
 #     print ("Welcome to Python programming.")
 # hello()
 
-def country_function(country = "Turkey "):
-    print( "I am from", country)
-country_function("Germany")
-country_function("USA")
-country_function("Dubai")
-country_function("Italy")
-country_function("Canada")
+# def country_function(country = "Turkey "):
+#     print( "I am from", country)
+# country_function("Germany")
+# country_function("USA")
+# country_function("Dubai")
+# country_function("Italy")
+# country_function("Canada")
+
+def furit_list():
+    return ["apple", "banana", "cherry", "date"]
+fruits = furit_list()
+print (fruits [0])
+print (fruits [2])
+print (fruits [3])
