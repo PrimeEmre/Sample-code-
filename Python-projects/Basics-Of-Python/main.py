44f152ba200307edc9bdb5e274640b0e004c827f
PrimeEmre <ynsemrgzl@gmail.com>
2025-12-18 13:48:21 -0500

    finshed the verible section

--- Python-projects/Basics-Of-Python/main.py
+++ Python-projects/Basics-Of-Python/main.py
@@ -3,10 +3,23 @@
 # print(100 + 100) 
 # print((200 + 500) *4 +200 - 890)
 
+#veribles 
+# num_1 = 100
+# num_2 = 500
+# print (num_1 + num_2)
+# name = "Emre"
+# age = 20
+# print ("My name is " + name + " and I am " + str(age) + " years old.")
+age = 30 
+name = "James Smith"
+job= " software developer"
+print  ("my name is" + " " + name +"."+ " " "I am "  + str(age) + " " + "years old." + " My job is" + job +"." )
+
+# lists in python
 # my_list = [10,20,30,40,50]
 # print (len(my_list))
 # fruit_list = ['apple:', 100, 'banana:', 50, 'cherry:',75]
 # print (fruit_list)
 
-car= list (( "bmw", "audi", "toyota", "ferrari"))
-print (car)
+# car= list (( "bmw", "audi", "toyota", "ferrari"))
+# print (car)
