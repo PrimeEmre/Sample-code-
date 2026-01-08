fc65ccf742d51e0c58e37f7d17b00e4677df0bb4
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2026-01-07 20:17:09 -0500

    finshed the code

--- Python-projects/Python-module/main.py
+++ Python-projects/Python-module/main.py
@@ -162,3 +162,7 @@
 #     # print("Success! og_image_generator.png has been created.")
 
 import cv2
+
+image = cv2.imread("dubai.png")
+height, width = image.shape[:2]
+print("Height = {}, Width = {}".format(height, width))
