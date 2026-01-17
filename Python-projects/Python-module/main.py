f4a7c7d3e28d54dd73b98da16a71f9895c4d3c8e
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2026-01-17 12:47:21 -0500

    test commit

--- Python-projects/Python-module/main.py
+++ Python-projects/Python-module/main.py
@@ -366,35 +366,35 @@ print(my_array)
 #
 # cap.release()
 # hand tracker 
-import cv2
-import mediapipe as mp
+# import cv2
+# import mediapipe as mp
 
-# Setting the MediaPipe and movements
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(max_num_hands=2)
-mp_draw = mp.solutions.drawing_utils
+# # Setting the MediaPipe and movements
+# mp_hands = mp.solutions.hands
+# hands = mp_hands.Hands(max_num_hands=2)
+# mp_draw = mp.solutions.drawing_utils
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
-    if not ret: break
+# while True:
+#     ret, frame = cap.read()
+#     if not ret: break
 
-    #Setting the RGB
-    img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = hands.process(img_rgb)
+#     #Setting the RGB
+#     img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     results = hands.process(img_rgb)
 
-    # Drawing the hands
-    if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+#     # Drawing the hands
+#     if results.multi_hand_landmarks:
+#         for hand_landmarks in results.multi_hand_landmarks:
+#             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-    cv2.imshow("Hand camera tracking" , frame)
+#     cv2.imshow("Hand camera tracking" , frame)
 
-    if cv2.waitKey(1) == ord('q'):
-        break
+#     if cv2.waitKey(1) == ord('q'):
+#         break
 
-# Numpy
+# Numpy array 
 import numpy as np
 
 my_array = np.array([100, 200, 300, 400, 500])
