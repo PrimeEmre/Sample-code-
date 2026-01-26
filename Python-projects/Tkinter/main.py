1bb2ea7fe6ab9270dce8a2f715a71f1f4b0848bf
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2026-01-26 10:30:25 -0500

    added sample tkinter

--- Python-projects/Tkinter/main.py
+++ Python-projects/Tkinter/main.py
@@ -1,1 +1,15 @@
-print("helloworld")
+import tkinter
+
+window = tkinter.Tk()
+window.title("Hello World GUI")
+window.minsize(width=1000, height=1000)
+window.config(padx=20, pady=20)
+
+my_label = tkinter.Label(text="Hello world", bg="black", fg="white", font=("Roboto", 24))
+my_label.pack()
+my_name_lable = tkinter.Label(text="Enter your name " ,bg="light blue" , fg="black", font=("Roboto" ,18))
+my_name_lable.pack(pady=12)
+my_entry = tkinter.Entry(bg="light green", fg="blue", font=("Roboto", 12))
+my_entry.pack(pady= 12)
+
+window.mainloop()
