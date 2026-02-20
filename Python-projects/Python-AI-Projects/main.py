49ac1283dddb09d93cc540e59b843f1e2bbe04b7
Emre Guzel <136666281+PrimeEmre@users.noreply.github.com>
2026-02-20 12:02:53 -0500

    trained  the 80% of the AI

--- Python-projects/Python-AI-Projects/main.py
+++ Python-projects/Python-AI-Projects/main.py
@@ -1,1 +1,17 @@
-print("hello , world")
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
+import pandas as pd
+
+# loding data
+iris = load_iris()
+
+# Addicng tables
+table = pd.DataFrame(iris.data, columns=iris.feature_names)
+table['species'] = iris.target
+
+print(table.head())
+
