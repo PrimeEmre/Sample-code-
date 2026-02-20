5a4cf4c18c676a1865081e3271a5fa0c893f93cd
PrimeEmre <ynsemrgzl@gmail.com>
2026-02-20 12:56:25 -0500

    finshed the code

--- Python-projects/Python-AI-Projects/main.py
+++ Python-projects/Python-AI-Projects/main.py
@@ -1,1 +1,23 @@
-print("hello , world")
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+# loding data
+iris = load_iris()
+x = iris.data
+y = iris.target
+
+# training the AI
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+print(f"Accuracy: {accuracy_score(y_test, predictions) * 100:.2f}%")
+
+# predicting new flower
+new_flower = np.array([[1.2, 4.6, 1.1, 7.5]])
+prediction = model.predict(new_flower)
+print(f"Predicted species: {iris.target_names[prediction][0]}")
