5fddd8d4afdad3cf0389bc9028e22ebb99cc855b
PrimeEmre <ynsemrgzl@gmail.com>
2026-09-18 11:54:40 -0400

    asnwerd the quesiton i have created

--- C-Programing/main.c
+++ C-Programing/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    printf( "Emre Guzel\n");
-    printf("Favorite place in the world?\n");
-    printf("Istanbul\n");
+    printf( "Favorite car? \n");
+    printf("Mercedes Maybach S class s 500 \n");
+    printf("\n");
+    printf("Favorite Resturan\n");
+    printf("Nusret\n");
+    printf("\n");
+    printf("Favorite food\n");
+    printf("Adana Kebab \n");
+
     return 0;
 }
