28b8f98669342ec16737a9b31dcc81f56e241397
PrimeEmre <ynsemrgzl@gmail.com>
2026-09-20 20:50:53 -0400

    almot finshing adding old programs

--- C-Programing/main.c
+++ C-Programing/main.c
@@ -1,3 +1,35 @@
+// Hello world program in C
+// #include <stdio.h>
+
+// int main(){
+//     printf("Hello, World!\n");
+//     return 0;
+
+// Priting our name 
+// #include <stdio.h>
+
+// int main(){
+//     printf( "Emre Guzel\n");
+//     printf("Favorite place in the world?\n");
+//     printf("Istanbul\n");
+//     return 0;
+// }
+
+// About us program
+// #include <stdio.h>
+
+// int main(){
+//     printf( "Favorite car? \n");
+//     printf("Mercedes Maybach S class s 500 \n");
+//     printf("\n");
+//     printf("Favorite Resturan\n");
+//     printf("Nusret\n");
+//     printf("\n");
+//     printf("Favorite food\n");
+//     printf("Adana Kebab \n");
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main(){
